4d4437e8aefe9f3cf1924dee1c89874e0689f1bb
jagadeesh-tolnut <jagadeeshmouthouvel@gmail.com>
2021-11-06 18:37:41 +0530

    nov 6,2021 practise

---
+++ hackerrank/pythonChallenge/Find the Runner-Up Score!.py
@@ -0,0 +1,10 @@
+# Nee sonadhu dhan
+# 100% Jansi logic
+# success
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr = list(set(arr))
+    arr.sort()
+    print(arr[-2])
+
