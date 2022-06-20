2ccddecfeec2087ba10747cba142c5b42d0da2ae
Jaga <jagadeeshmouthouvel@gmail.com>
2022-06-20 10:40:57 +0530

    pattern 1 modified

--- pattern/pattern1.py
+++ pattern/pattern1.py
@@ -2,3 +2,27 @@ n = int(input())
 for i in range(n):
     print("n "*n)
     
+"""
+
+5
+n n n n n 
+n n n n n 
+n n n n n 
+n n n n n 
+n n n n n 
+
+
+10
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+n n n n n n n n n n 
+
+
+"""
