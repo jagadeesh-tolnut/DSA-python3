0e5e1829ee288c3db03d503c6496b6caf5d5da24
Jaga <jagadeeshmouthouvel@gmail.com>
2022-06-21 15:31:19 +0530

    pattern 19 solved

---
+++ pattern/pattern19.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+for i in range(1,n+1):
+
+    for j in range(i):
+        print("*",end="")
+    for k in range(2*n-(2*i)):
+        print(" ",end="")
+    for l in range(i):
+        print("*",end="")
+    print()
+
+
+for i in range(n-1,0,-1):
+
+    for j in range(i):
+        print("*",end="")
+    for k in range(2*n-(2*i)):
+        print(" ",end="")
+    for l in range(i):
+        print("*",end="")
+    print()
