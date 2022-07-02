f2eecb665f1edec5effb23b6b39cdd46045bedc3
Jaga <jagadeeshmouthouvel@gmail.com>
2022-07-03 00:17:45 +0530

    Pattern X print sollution

---
+++ zoho-prep/round2/solutions/PrintPatterX.py
@@ -0,0 +1,17 @@
+st = input()
+l = len(st)
+half = l//2
+for i in range(half):
+    print(" "*i,end="")
+    print(st[i],end="")
+    print(" "*(l-(2*i)-1),end="")
+    print(st[l-i-1])
+
+print(" "*half,end="")
+print(st[half])
+
+for i in range(half-1,-1,-1):
+    print(" " * i, end="")
+    print(st[i], end="")
+    print(" " * (l - (2 * i)-1), end="")
+    print(st[l - i-1])
