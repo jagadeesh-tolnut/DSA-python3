42edeb8d7246ac250b4d83b39cea35f6217b4ca7
Jaga <jagadeeshmouthouvel@gmail.com>
2022-09-21 23:36:34 +0530

    Two sum

--- leetcode/1. Two Sum.py
+++ leetcode/1. Two Sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hp = {}
+        for i, n in enumerate(nums):
+            dif = target - n
+            if dif in hp:
+                return [hp[dif], i]
+            hp[n] = i
