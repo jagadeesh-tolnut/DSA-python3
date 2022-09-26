eefcce7c90e9579ba8429177b8859b9e741127e8
Jaga <jagadeeshmouthouvel@gmail.com>
2022-09-27 00:55:44 +0530

    Binary Search

---
+++ leetcode/704. Binary Search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while (start <= end):
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
