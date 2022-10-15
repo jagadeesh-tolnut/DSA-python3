623e6e57a955c1647a2cb6eebc7aa28909e6b537
Jaga <jagadeeshmouthouvel@gmail.com>
2022-10-15 17:39:55 +0530

    Valid Sudoku

---
+++ leetcode/36. Valid Sudoku.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        subbox = collections.defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if (board[i][j] in rows[i]
+                        or board[i][j] in cols[j]
+                        or board[i][j] in subbox[(i // 3, j // 3)]):
+                    return False
+
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                subbox[(i // 3, j // 3)].add(board[i][j])
+
+        return True
