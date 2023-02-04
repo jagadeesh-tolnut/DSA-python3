53fc161b2671382882301bf209e8ea2d0485c154
Jaga <jagadeeshmouthouvel@gmail.com>
2023-02-04 23:58:12 +0530

    Reverse string

---
+++ leetcode/344. Reverse String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l,r = 0,len(s)-1
+        while l<r:
+            s[l],s[r] = s[r], s[l]
+            l=l+1
+            r=r-1
